2eb5b9c98fb42d5ae3d33fb5b7dd93ccba33c0bb
Felix Grant <fgrant505@outlook.com>
2023-09-11 14:30:30 +0100

    Print board function

--- chess.py
+++ chess.py
@@ -6,26 +6,70 @@ class Chess():
             self.__dict__ = json.loads(file.read())
 
             self.activeColour = 0 if self.fen.split()[1] == 'w' else 1
-            self.printBoard(self.fen.split()[0])
+            self.FormatBoard(self.fen.split()[0])
 
-    def printBoard(self, fenNotation: str):
+    def PrintBoard(self):
         boardList = list()
-        fenNotation = fenNotation.replace('/', '\n')
-        for char in fenNotation:
-            # Append a space before each square for display
+        file = 0
+        for char in self.board:
+            # Append a space before each square for display purposes
             boardList.append(' ')
             
-            # Replace the numbers in FEN notation with that many dots
+            # Print the unicode symbol for each square
+            boardList.append(self.pieces[char])
+                
+            # Append a newline character if you've reached the end of the file
+            if (file % 7 == 0 and file != 0):
+                boardList.append('\n')
+                file = 0
+            else:
+                file += 1
+
+        print(''.join(boardList))
+        
+    def FormatBoard(self, fenNotation: str):
+        board = list()
+
+        fenNotation = fenNotation.replace('/', '')
+        
+        for char in fenNotation:
             if char.isdigit():
                 # Replace the numbers in FEN notation with that many dots
                 for i in range(int(char)):
-                    boardList.append('.' + ' ')
+                    board.append('.')
             else:
-                # Replace piece letters with their unicode symbol
-                boardList.append(self.pieces[char])
+                board.append(char)
 
-        # Join the board list into a multi-line string
-        board = ''.join(boardList)
-        print(board)
+        self.board = ''.join(board)
+        
+    def GenerateMoveList(self):
+        moveList = []
+        for square in range(len(self.board)):
+            piece = self.board[square]
+            # Only consider pieces of the current players colour
+            if piece != '.' and self.colours[piece] == self.activeColour:
+                print(square)
+                for offset in self.directions[piece]:
+                    targetSquare = square
+                    while True:
+                        targetSquare += offset 
+                        capturedPiece = self.board[targetSquare]
+                        
+                        # If the target square contains a piece of the current
+                        # players colour, then this offset can no longer be 
+                        # traversed
+                        if self.colours[capturedPiece] == self.activeColour: break
+                        
+                        # Check that the target square is actually on the board
+                        if 
+                    # Break if the target square is occupied by a piece of the players
+                    # colour
+                    
+        return moveList
+    
+    def Play(self):
+        self.GenerateMoveList()
 
-chess = Chess("settings.json")
+chess = Chess('settings.json')
+chess.PrintBoard()
+chess.Play()
